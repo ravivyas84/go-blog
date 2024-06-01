829ea0118222469b5294b5df49ec63e853c0ac70
Ravi Vyas <github@ravivyas.com>
2024-06-01 22:48:52 +0530

    added pages and post lists

--- main.go
+++ main.go
@@ -30,6 +30,18 @@ type Document struct {
 	Headings    []string
 }
 
+type LatestPosts struct {
+	FrontMatter frontmatter.FrontMatter
+	Content     template.HTML
+	Latest      []Post
+}
+
+type Post struct {
+	Title string
+	Slug  string
+	Date  time.Time
+}
+
 type RSS struct {
 	XMLName xml.Name `xml:"rss"`
 	Version string   `xml:"version,attr"`
@@ -134,9 +146,12 @@ func main() {
 			continue
 		}
 
+		// Generate slug
+		slug := generateSlug(fm.Date, fm.Slug)
+
 		// Insert post data into the SQLite database
-		_, err = db.Exec("INSERT INTO posts (title, content, pub_date, headings) VALUES (?, ?, ?, ?)",
-			fm.Title, buf.String(), fm.Date, headingsJSON)
+		_, err = db.Exec("INSERT INTO posts (title, content, pub_date, headings,slug) VALUES (?, ?, ?, ?, ?)",
+			fm.Title, buf.String(), fm.Date, headingsJSON, slug)
 		if err != nil {
 			log.Printf("error inserting post data into database for file %s: %v", filePath, err)
 			continue
@@ -147,6 +162,12 @@ func main() {
 
 	// Generate pages from database
 	generatePagesFromDB(db, buildDir)
+
+	// Generate static
+	buildPages(db)
+
+	// Generate posts page from database
+	listAllPosts(db, buildDir)
 
 	// Serialize RSS to XML
 	outputRSS, err := xml.MarshalIndent(rss, "", "  ")
@@ -167,6 +188,17 @@ func main() {
 	}
 
 	log.Printf("Successfully copied %s to %s", src, dst)
+}
+
+// Generate the slug
+func generateSlug(dateStr, frontmatterSlug string) string {
+	parsedDate, err := time.Parse("2006-01-02", dateStr)
+	if err != nil {
+		log.Printf("error parsing date: %v", err)
+		return ""
+	}
+	slug := fmt.Sprintf("%d/%02d/%02d/%s", parsedDate.Year(), parsedDate.Month(), parsedDate.Day(), frontmatterSlug)
+	return slug
 }
 
 func initDB(dbPath string) (*sql.DB, error) {
@@ -190,7 +222,8 @@ func initDB(dbPath string) (*sql.DB, error) {
 			title TEXT,
 			content TEXT,
 			pub_date TEXT,
-            headings TEXT NULL
+            headings TEXT NULL,
+            slug TEXT
 	);`
 
 	_, err = db.Exec(createTableSQL)
@@ -202,22 +235,22 @@ func initDB(dbPath string) (*sql.DB, error) {
 }
 
 func generatePagesFromDB(db *sql.DB, buildDir string) {
-	rows, err := db.Query("SELECT title, content, pub_date, headings FROM posts")
+	rows, err := db.Query("SELECT title, content, pub_date, headings,slug FROM posts")
 	if err != nil {
 		log.Fatalf("error querying posts from database: %v", err)
 	}
 	defer rows.Close()
 
 	for rows.Next() {
-		var title, content, pubDate, headingsJSON string
-		err := rows.Scan(&title, &content, &pubDate, &headingsJSON)
+		var title, content, pubDate, headingsJSON, slug string
+		err := rows.Scan(&title, &content, &pubDate, &headingsJSON, &slug)
 		if err != nil {
 			log.Printf("error scanning post data: %v", err)
 			continue
 		}
 
 		// Parse date from front matter
-		parsedDate, err := time.Parse("2006-01-02", pubDate)
+		// parsedDate, err := time.Parse("2006-01-02", pubDate)
 		if err != nil {
 			log.Printf("error parsing date from front matter for file %s: %v", title, err)
 			continue
@@ -235,7 +268,7 @@ func generatePagesFromDB(db *sql.DB, buildDir string) {
 		fmt.Printf("Unmarshalled headings for post '%s'::::::: %v\n", title, headings)
 
 		// Generate output path based on date
-		outputPath := filepath.Join(buildDir, fmt.Sprintf("%d/%02d/%02d", parsedDate.Year(), parsedDate.Month(), parsedDate.Day()), "index.html")
+		outputPath := filepath.Join(buildDir, slug, "index.html")
 
 		// Create output directory if it doesn't exist
 		outputDir := filepath.Dir(outputPath)
@@ -295,4 +328,184 @@ func extractHeadings(htmlContent string) []string {
 	}
 	f(doc)
 	return headings
+}
+
+func buildPages(db *sql.DB) {
+	pagesDir := "pages"
+	buildDir := "build"
+	files, err := os.ReadDir(pagesDir)
+	if err != nil {
+		log.Fatalf("error reading pages directory: %v", err)
+	}
+
+	for _, file := range files {
+		if file.IsDir() {
+			continue // Skip directories
+		}
+
+		// Read the content of the file
+		filePath := filepath.Join(pagesDir, file.Name())
+		content, err := os.ReadFile(filePath)
+		if err != nil {
+			log.Printf("error reading file %s: %v", filePath, err)
+			continue
+		}
+
+		// Parse front matter and content
+		parts := bytes.SplitN(content, []byte("---"), 3)
+		var fm frontmatter.FrontMatter
+		if err := yaml.Unmarshal(parts[1], &fm); err != nil {
+			log.Printf("error parsing front matter for file %s: %v", file.Name(), err)
+			continue
+		}
+
+		// Convert markdown to HTML
+		var buf bytes.Buffer
+
+		if err := goldmark.Convert(parts[2], &buf); err != nil {
+			log.Printf("error converting markdown for file %s: %v", filePath, err)
+			continue
+		}
+		// Handle the index.md differently
+		var latest_Posts []Post
+
+		outputPath := filepath.Join(buildDir, file.Name())
+		outputPath = strings.TrimSuffix(outputPath, filepath.Ext(outputPath)) + "/index.html"
+
+		if file.Name() == "index.md" {
+			outputPath = filepath.Join(buildDir, file.Name())
+			outputPath = strings.TrimSuffix(outputPath, filepath.Ext(outputPath)) + ".html"
+			latest_Posts, err = fetchLatestPosts(db)
+			if err != nil {
+				log.Printf("error fetching latest posts: %v", err)
+				continue
+			}
+		}
+
+		outputDir := filepath.Dir(outputPath)
+		if err := os.MkdirAll(outputDir, 0755); err != nil {
+			log.Printf("error creating output directory %s: %v", outputDir, err)
+			continue
+		}
+
+		doc := LatestPosts{
+			FrontMatter: fm,
+			Content:     template.HTML(buf.String()),
+			Latest:      latest_Posts,
+		}
+
+		tmpl := template.New("base.tmpl")
+		tmpl, err = tmpl.ParseFiles("footer.tmpl", "header.tmpl", "pages.tmpl", "base.tmpl")
+		if err != nil {
+			log.Printf("error parsing template files: %v", err)
+			continue
+		}
+
+		outputFile, err := os.Create(outputPath)
+		if err != nil {
+			log.Printf("error creating output file %s: %v", outputPath, err)
+			continue
+		}
+		defer outputFile.Close()
+
+		if err := tmpl.ExecuteTemplate(outputFile, "base.tmpl", doc); err != nil {
+			log.Printf("error executing template for file %s: %v", outputPath, err)
+			continue
+		}
+
+		fmt.Printf("Processed page %s, output %s\n", file.Name(), outputPath)
+	}
+}
+
+func fetchLatestPosts(db *sql.DB) ([]Post, error) {
+	rows, err := db.Query("SELECT title, slug, pub_date FROM posts ORDER BY pub_date DESC LIMIT 10")
+	if err != nil {
+		return nil, fmt.Errorf("error querying latest posts: %v", err)
+	}
+	defer rows.Close()
+
+	var posts []Post
+	for rows.Next() {
+		var post Post
+		var pubDate string
+		err := rows.Scan(&post.Title, &post.Slug, &pubDate)
+		if err != nil {
+			return nil, fmt.Errorf("error scanning post data: %v", err)
+		}
+
+		post.Date, err = time.Parse("2006-01-02", pubDate)
+		if err != nil {
+			return nil, fmt.Errorf("error parsing date: %v", err)
+		}
+
+		posts = append(posts, post)
+	}
+
+	return posts, nil
+}
+
+func listAllPosts(db *sql.DB, buildDir string) {
+	// Fetch all posts from the database
+	rows, err := db.Query("SELECT title, slug, pub_date FROM posts ORDER BY pub_date DESC")
+	if err != nil {
+		log.Fatalf("error querying all posts: %v", err)
+	}
+	defer rows.Close()
+
+	var posts []Post
+	for rows.Next() {
+		var post Post
+		var pubDate string
+		err := rows.Scan(&post.Title, &post.Slug, &pubDate)
+		if err != nil {
+			log.Printf("error scanning post data: %v", err)
+			continue
+		}
+
+		post.Date, err = time.Parse("2006-01-02", pubDate)
+		if err != nil {
+			log.Printf("error parsing date: %v", err)
+			continue
+		}
+
+		posts = append(posts, post)
+	}
+
+	// Generate the HTML for the list of all posts
+	outputPath := filepath.Join(buildDir, "posts", "index.html")
+
+	outputDir := filepath.Dir(outputPath)
+	if err := os.MkdirAll(outputDir, 0755); err != nil {
+		log.Printf("error creating output directory %s: %v", outputDir, err)
+		return
+	}
+
+	doc := struct {
+		Title string
+		Posts []Post
+	}{
+		Title: "Blog",
+		Posts: posts,
+	}
+
+	tmpl := template.New("base_2.tmpl")
+	tmpl, err = tmpl.ParseFiles("footer.tmpl", "header.tmpl", "posts.tmpl", "base_2.tmpl")
+	if err != nil {
+		log.Printf("error parsing template files: %v", err)
+		return
+	}
+
+	outputFile, err := os.Create(outputPath)
+	if err != nil {
+		log.Printf("error creating output file %s: %v", outputPath, err)
+		return
+	}
+	defer outputFile.Close()
+
+	if err := tmpl.ExecuteTemplate(outputFile, "base_2.tmpl", doc); err != nil {
+		log.Printf("error executing template for file %s: %v", outputPath, err)
+		return
+	}
+
+	fmt.Printf("Processed all posts, output %s\n", outputPath)
 }
