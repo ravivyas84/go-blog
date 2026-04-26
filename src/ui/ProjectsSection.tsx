3095be6a851f0aee0329334c599019e69724fc86
Ravi Vyas <github@ravivyas.com>
2026-04-26 13:28:36 +0530

    Add heading section to ProjectsSection with link to all projects

--- src/ui/ProjectsSection.tsx
+++ src/ui/ProjectsSection.tsx
@@ -1,12 +1,19 @@
 import { featuredProjectItems } from '../lib/site-profile';
 
+import { Icon } from './Icon';
 import { ProjectCardConcept } from './ProjectCardConcept';
 import { SectionTitle } from './SectionTitle';
 
 export function ProjectsSection() {
   return (
     <section className="projects-section" id="projects">
-      <SectionTitle icon="projects" title="Projects and Tools" />
+      <div className="projects-section__heading">
+        <SectionTitle icon="projects" title="Projects and Tools" />
+        <a className="projects-section__heading-link" href="/projects-and-tools/">
+          <span>View all projects</span>
+          <Icon name="arrow-right" className="projects-section__heading-link-icon" />
+        </a>
+      </div>
       <div className="projects-section__carousel" aria-label="Projects and tools">
         {featuredProjectItems.map((project) => (
           <ProjectCardConcept key={project.title} {...project} />
